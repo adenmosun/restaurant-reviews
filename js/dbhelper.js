3a3cc1746a1adcbbacfa298fc8ce92f2813674af
temitayo <tayo.adenmosun@gmail.com>
2018-08-16 22:25:56 +0100

    fix: implementation of responsive images in dbhelper.js, main.js and restaurant_info.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -151,7 +151,12 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    const image_path = `./img/${restaurant.photograph.substr(0, 1)}`;
+    return {
+      small: `${image_path}-small.jpg`,
+      medium: `${image_path}-medium.jpg`,
+      large: `${image_path}-large.jpg`
+    };
   }
 
   /**
@@ -160,7 +165,7 @@ class DBHelper {
    static mapMarkerForRestaurant(restaurant, map) {
     // https://leafletjs.com/reference-1.3.0.html#marker  
     const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng],
-      {title: restaurant.name,
+      { title: restaurant.name,
       alt: restaurant.name,
       url: DBHelper.urlForRestaurant(restaurant)
       })
