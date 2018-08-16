e26dee80c678690bf22c52aa9b6dd84d95e73b28
temitayo <tayo.adenmosun@gmail.com>
2018-08-16 15:33:07 +0100

    chore: Include dependencies, image compression

---
+++ sw.js
@@ -0,0 +1,76 @@
+
+const cache_name = 'restaurant-reviews-v1';
+const cache_items = [
+        "./",
+        "./index.html",  
+        "./restaurant.html",
+        "./css/styles.css",
+        "./js/dbhelper.js",
+        "./js/main.js",
+        "./js/restaurant_info.js",
+        "./img/1.jpg",
+        // "/.img/2.jpg",
+        "./img/3.jpg",
+        "./img/4.jpg",
+        "./img/5.jpg",
+        "./img/6.jpg",
+        "./img/7.jpg",
+        "./img/8.jpg",
+        "./img/9.jpg",
+        "./img/10.jpg"
+      ]
+
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cache_name)
+      .then((cache) => cache.addAll(cache_items))
+  )
+});
+
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(key => {
+          if (key !== cache_name) {
+            return caches.delete(key);
+          }
+        }),
+      ),
+    ),
+  );
+});
+
+
+addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;     
+          
+        } else {
+          return fetch(event.request)   
+          
+            .then(res => caches.open(cache_name)
+            .then(cache => {
+              cache.put(event.request.url, res.clone());   
+            
+              return res;  
+            }))
+            .catch(err => caches.open("Error")
+            .then(cache => cache.match(cache_name)));
+        }
+      })
+  );
+});         
+
+
+
+self.addEventListener('message', (event) => {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
